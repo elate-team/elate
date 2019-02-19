9e73389c80d5b0127cfdcf0247c8362c475788f2
Jan Mikeš <j.mikes@me.com>
2019-02-19 01:41:08 +0100

    Creating lead entity

--- api/src/Entity/Lead.php
+++ api/src/Entity/Lead.php
@@ -2,13 +2,54 @@
 
 namespace Elate\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\UuidInterface;
 
 /**
  * @ORM\Entity
  */
 class Lead
 {
+    /**
+     * @var UuidInterface
+     * @ORM\Id
+     * @ORM\Column(type="uuid")
+     */
+    private $id;
 
+    /**
+     * @ORM\Column(type="string", nullable=TRUE)
+     * @var string|null
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", nullable=TRUE)
+     * @var string|null
+     */
+    private $phone;
+
+
+    public function __construct(UuidInterface $id)
+    {
+        $this->id = $id;
+    }
+
+
+    public function id(): UuidInterface
+    {
+        return $this->id;
+    }
+
+
+    public function email(): ?string
+    {
+        return $this->email;
+    }
+
+
+    public function phone(): ?string
+    {
+        return $this->phone;
+    }
 }
---
+++ api/src/Migrations/2019/02/Version20190219003931.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190219003931 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Added lead entity';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE TABLE lead (id UUID NOT NULL, email VARCHAR(255) DEFAULT NULL, phone VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN lead.id IS \'(DC2Type:uuid)\'');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP TABLE lead');
+    }
+}
