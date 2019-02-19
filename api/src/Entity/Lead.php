5ee19f2d91166cfe63190dedc2a68bee4d7f5f4d
Jan Mikeš <j.mikes@me.com>
2019-02-19 01:33:42 +0100

    Installed Ramsey Uuid

---
+++ api/src/Entity/Lead.php
@@ -0,0 +1,14 @@
+<?php declare (strict_types=1);
+
+namespace Elate\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class Lead
+{
+
+}
